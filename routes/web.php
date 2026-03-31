12ba40529d623954fac10e4170e959354329b796
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2026-03-31 23:02:41 +0545

    Created Simple CRUD Outline

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,25 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+use App\Models\Teacher;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('teachers',function (){
+//     return Teacher::all();
+// });
 
-Route::get("/aboutus", function () {
-    $name = "tester";
-    $email="tester@email.com";
-    #return view('aboutus')->with('name', $name)->with('email', $email);      -----> Method 1 of sending data to frontend
-    #return view("aboutus", compact("name","email")); #-------> Method 2 of sending data to frontend (view)
-    return view('aboutus', ['name'=>$name, 'email'=>$email]); #------------>Method 3 of sending data
-});
+Route::get('teachers', [TeacherController::class,'index']);
 
-## GROUPING ROUTES
-Route::prefix("details")->group(function(){
-    Route::get('/students', function(){
-        return "This is students detail";
-    }) -> name("student-Detail");
-    Route::get('/teacher', function(){
-        return "This is teacher detail";
-    }) -> name("teacher-Detail");
-} );
+Route::get('add-teacher', [TeacherController::class,'add']);
 
+Route::get('show-teacher/{id}',[ TeacherController::class,'show']);
 
-#PATH PARAM IN LARAVEL
-Route::get('student/{id}/{reg}', function($id, $reg){
-    return 'Student number '.$id." Regestration ".$reg;
-});
+Route::get('update-teacher/{id}',[ TeacherController::class,'update']);
 
-Route::fallback(function(){
-    return "Page is not fount";
-});
+Route::get('delete-teacher/{id}',[ TeacherController::class,'delete']);
 
-
-# DIRECTLY CALLING VIEW
-
-Route::view("contact-us","contactus", ["name"=>"Mandeep", "email"=> "mandeep@gmail.com  "]);
-
-#Calling controller
-Route::controller(StudentController::class)->group(function(){
-    Route::get("students",'index');
-    Route::get("about-us/{id}/{name}",'aboutUs');
-});
